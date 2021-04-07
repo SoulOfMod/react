c0cf80ccf08380d35154ef2a2c7f02a44da7136c
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-04-07 15:52:50 +0200

    elsy etape 1-2-3

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import './App.css';
 
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="text-primary">Hello !</h1>
+        <div className="row">
+          <p>Temperature minimal : {tempMin}</p>
+          <p>Temperature maximal : {tempMax}</p>
+          <p>Battement minute minimal : {heartMin}</p>
+          <p>Battement minute maximal : {heartMax}</p>
+          <p>Nombre de Pas minimal : {stepsMin}</p>
+          <p>Nombre de Pas maximal : {stepsMax}</p>
+        </div>
       </div>
     );
   }
---
+++ chapitre_01/elsy/src/components/Steps.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../App.js';
+
+class steps extends React.Component {
+
+
+  render() {
+    return (
+      <div className= "box col-sm-3 col-6">
+        test <MdDirectionsWalk/>
+      </div>
+    );
+  }
+
+}
+
+export default Steps;
