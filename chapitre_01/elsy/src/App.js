09708d48c9dca52df13baf97406790c8f0699d88
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-04-07 17:26:02 +0200

    elsy finis

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import './App.css';
-import Steps from './components/Steps.jsx'
-
-
+import Box from './components/Box.jsx'
+import './styles/global.css'
 const tempMin = -20
 const tempMax = 40
 const heartMin = 80
@@ -15,15 +14,17 @@ class App extends React.Component {
     return (
       <div className="container-fluid">
         <div className="row">
-          < Steps />
+        < Box color="#3A85FF" icon="local_drink" value={1.5} unit="L" />
+        < Box color="black" icon="directions_walk" value={3000} unit="steps" />
+        < Box color="red" icon="favorite" value={120} unit="bpm  " />
+        < Box color="yellow" icon="wb_sunny" value={-10} unit="C°" />
           <p>Heart  : {heartMin}</p>
           <p>Temperature   : {tempMin}</p>
           <p>Steps  : {stepsMin}</p>
-          </div>
-          </div>
+        </div>
+      </div>
     );
   }
 }
 
 export default App;
-  
