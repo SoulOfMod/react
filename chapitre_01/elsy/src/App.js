ba97f04d251aa2aed27edd9e6c38b1456bb893bd
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-04-12 17:20:18 +0200

    elsy 2

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -9,15 +9,39 @@ const heartMax = 180
 const stepsMin = 0
 const stepsMax = 50000
 
+
+
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+      value: 0
+    }
+  }
+
+  onHeartChange = () => {
+    this.setState({ heart: this.state.value });
+  }
+  
+
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-        < Box color="#3A85FF" icon="local_drink" value={1.5} unit="L" />
-        < Box color="black" icon="directions_walk" value={3000} unit="steps" />
-        < Box color="red" icon="favorite" value={120} unit="bpm  " />
-        < Box color="yellow" icon="wb_sunny" value={-10} unit="C°" />
+          {/* Water */}
+          < Box color="#3A85FF" icon="local_drink" value={1.5} unit="L" />
+          {/* Steps */}
+          < Box color="black" icon="directions_walk" value={3000} unit="steps" />
+          {/* Heart */}
+          < Box color="red" icon="favorite" value={120} unit="bpm" />
+          {/* Sun */}
+          < Box color="yellow" icon="wb_sunny" value={-10} unit="C°" />
           <p>Heart  : {heartMin}</p>
           <p>Temperature   : {tempMin}</p>
           <p>Steps  : {stepsMin}</p>
--- chapitre_01/elsy/src/components/Box.jsx
+++ chapitre_01/elsy/src/components/Box.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
+
+const onHeartChange = () => {
+  this.setState({ heart: this.state.value });
+}
+
 class Box extends React.Component {
 
 
 
   render() {
+
+    const slider = () => {
+        if (this.props.unit !== "L") {
+          return (<input type="range" min={this.props.stepsMin} max={this.props.stepsMax} value={this.props.value} oninput></input>)
+        }
+    };
+
+
     return (
       <div id="Box" className="box col-sm-3 col-6">
         <span className="material-icons" style={{ fontSize: 100, color: this.props.color }}
         >{this.props.icon}</span>
-      <p>{this.props.value} {this.props.unit}</p>
+        <p>{this.props.value} {this.props.unit}</p>
+        {slider()}
       </div>
     );
   }
