83ed32fc94418d2d1b0df1dc0ed30dc491a721fb
SoulOfMod <72465476+SoulOfMod@users.noreply.github.com>
2021-04-14 16:56:26 +0200

    bakery in the making

--- chapitre_04/bakery/src/App.js
+++ chapitre_04/bakery/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import './App.css';
 import './styles/global.css'
-import './List'
-import './Add'
-import './Pay'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -12,23 +9,26 @@ class App extends React.Component {
 
   constructor() {
     super()
-
+    this.state = {
+      activeTab: "add",
+      items: {}
+    }
   }
 
 
   render() {
-      return (
-        <div>
-          <h1>Bakery</h1>
-          <ul className="header">
-            <li><a href="/add">Add</a></li>
-            <li><a href="/list">List</a></li>
-            <li><a href="/pay">Pay</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
+    return (
+      <div>
+        <h1>Bakery</h1>
+        <ul className="header">
+          <li><button className="buttonheader">Add</button></li>
+          <li><button className="buttonheader">List</button></li>
+          <li><button className="buttonheader">Pay</button></li>
+        </ul>
+        <div className="content">
+
         </div>
+      </div>
     );
   }
 }
